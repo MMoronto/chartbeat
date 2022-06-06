045da7ad8fee7600ec30b8a2b12cc3bc0aa0e960
MMORONTO <t2wycemo@gmail.com>
2022-06-06 14:34:52 -0500

    Set up App.js components and style elements / components

--- src/App.js
+++ src/App.js
@@ -6,21 +6,31 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css'
 
 const App = () => {
+    const activeMenu = true;
   return (
     <div>
         <BrowserRouter>
             <div className='flex relative dark:bg-main-dark-bg'>
                 <div className='fixed right-4 bottom-4' style={{ zIndex: '1000'}}>
                     <TooltipComponent content="Settings" position="Top">
-                        <button>
+                        <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{background: 'blue', borderRadius: '50%' }}>
                             <FiSettings />
                         </button>
                     </TooltipComponent>
                 </div>
+                {activeMenu ? (
+                    <div>
+                        Sidebar
+                    </div>
+                ) : (
+                    <div>
+                        Sidebar w-0
+                    </div>
+                )}
             </div>
         </BrowserRouter>
-    </div>>
-  )
+    </div>
+ )
 }
 
 export default App
