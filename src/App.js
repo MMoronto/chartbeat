7fffbc358d79f46254efcf231a2c375d0ac0ec95
MMORONTO <t2wycemo@gmail.com>
2022-06-06 14:12:04 -0500

    style components: buttons, tooltip

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
         <BrowserRouter>
             <div className='flex relative dark:bg-main-dark-bg'>
                 <div className='fixed right-4 bottom-4' style={{ zIndex: '1000'}}>
-                    <TooltipComponent>
+                    <TooltipComponent content="Settings" position="Top">
                         <button>
                             <FiSettings />
                         </button>
