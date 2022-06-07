cc0660e938d6168c1dba3f30b9f3a7159445d872
MMORONTO <t2wycemo@gmail.com>
2022-06-07 15:02:19 -0500

    setup navbar element. style element

--- src/App.js
+++ src/App.js
@@ -30,7 +30,9 @@ const App = () => {
                 <div className={ 
                     `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72 w-full' : 'flex-2'}` 
                 }>
-                    
+                   <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+                       Navbar
+                   </div> 
                 </div>
             </div>
         </BrowserRouter>
