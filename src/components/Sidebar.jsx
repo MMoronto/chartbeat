6e5f4f792f27725db69b42f07f20ada7c511698c
MMORONTO <t2wycemo@gmail.com>
2022-06-09 15:30:20 -0500

    set up sidebar. style component elements, buttons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,9 +13,12 @@ const Sidebar = () => {
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
       {activeMenu && (<>
         <div className='flex justify-between items-center'>
-          <Link to='/' onClick={() => {}} className='items-center'>
-            <SiShopware className='text-3x1' />
+          <Link to='/' onClick={() => {}} className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'>
+            <SiShopware /> <span>Bartar</span>
           </Link>
+          <TooltipComponent content='Menu' position='BottomCenter'>
+            <button></button>
+          </TooltipComponent>
         </div>
       </>)}
     </div>
