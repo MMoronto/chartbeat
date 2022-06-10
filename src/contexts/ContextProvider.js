78a6d08eb6a0d9290ad991c0c44b43507b9995a1
MMORONTO <t2wycemo@gmail.com>
2022-06-10 14:31:59 -0500

    set up context API component

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,1 +1,9 @@
 import React, { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext();
+
+const initialState = {
+    chat: false,
+    chart: false,
+    userProfile: false
+}
