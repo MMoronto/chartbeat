21e39499087fbb3f93a9db50c6f485ce066a4cb2
MMORONTO <t2wycemo@gmail.com>
2022-06-10 11:44:16 -0500

    navbar component style mods

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -30,7 +30,7 @@ const Sidebar = () => {
         </div>
         <div className='mt-10'>
           {links.map((item) => (
-            <div key={item.title}>
+            <div className='text-gray-400 m-3 mt-4 uppercase' key={item.title}>
               <p>
                 {item.title}
               </p>
