406fedc25bb0eb37f66824e63a515482e16e3a8c
MMORONTO <t2wycemo@gmail.com>
2022-06-10 14:42:48 -0500

    set up contextProvider StateContext params

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -5,5 +5,13 @@ const StateContext = createContext();
 const initialState = {
     chat: false,
     chart: false,
-    userProfile: false
+    userProfile: false,
+}
+
+export const ContextProvider = ({ children }) => {
+    return <StateContext.Provider
+        value={{test: 'test'}}
+    >
+        {children}
+    </StateContext.Provider>
 }
