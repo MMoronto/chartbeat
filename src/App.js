f831e8b5b65b64a8645880af9aecc50d68c2d7f7
MMORONTO <t2wycemo@gmail.com>
2022-06-11 12:55:59 -0500

    import useStateContext hook into App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { useStateContext } from './contexts/ContextProvider';
 import './App.css'
 
 const App = () => {
-    const activeMenu = true;
+  const { activeMenu } = useStateContext();
   return (
     <div>
         <BrowserRouter>
