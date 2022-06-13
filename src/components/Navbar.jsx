e4a41d2405dddd05526636f10d1060db7f4c457f
MMORONTO <t2wycemo@gmail.com>
2022-06-13 15:31:23 -0500

    fix navbar /button functionality issue.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,8 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
   <TooltipComponent content={title} position='BottomCenter'>
     <button type='button' onClick={customFunc} 
     style={{ color }} 
-    className='relative text-xl rounded-full p-3 hover:bg-light-gray'>
+    className='relative text-xl rounded-full p-3 hover:bg-light-gray'
+    >
       <span style={{ background: dotColor }}
         className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'
       >
@@ -24,8 +25,7 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 )
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu } =
-  useStateContext();
+  const { activeMenu, setActiveMenu } = useStateContext();
 
   return (
     <div className='flex justify-between p-2 md:mx-6 relative'>
