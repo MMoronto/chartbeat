c238df0b0be8d97972b70f14502cd9e88fa251c5
MMORONTO <t2wycemo@gmail.com>
2022-06-13 11:35:41 -0500

    create tooltip component for navButton

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,9 @@ import { Cart, Chat, Notification, UserProfile } from './';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
-
+  <TooltipComponent content={title} position='BottomCenter'>
+    <button></button>
+  </TooltipComponent>
 )
 
 const Navbar = () => {
