b3462539c210e234842632415a77a210ad4a5b9a
MMORONTO <t2wycemo@gmail.com>
2022-06-17 11:22:20 -0500

    setup ecommerce.jsx component. import components, files, date

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { FiSettings } from 'react-icons/fi';
+import { GoPrimitiveDot } from 'react-icons/go';
+
+import { stacked, Pie, Button, SparkLine } from '../components';
+
+import { earningData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Ecommerce = () => {
   return (
