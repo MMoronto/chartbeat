99f878ee67cd1867c5fa8281eeb31dd59d47f5f7
MMORONTO <t2wycemo@gmail.com>
2022-06-18 07:10:48 -0500

    define sparkLine chart properties & fetch data from source

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-const SparkLine = ({ id, height, width, color, data, type, currentCOLOR }) => {
+const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
   return (
-    <SparklineComponent >
-      
+    <SparklineComponent 
+      id={id}
+      height={height}
+      width={width}
+      linewidth={1}
+      valueType='Numeric'
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+    >
+      <Inject services={[SparklineTooltip]} />
     </SparklineComponent>
   )
 }
