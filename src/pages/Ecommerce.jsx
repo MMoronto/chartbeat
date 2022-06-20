97836dfa268958a882691bdc7756b12075b99b22
MMORONTO <t2wycemo@gmail.com>
2022-06-20 12:41:32 -0500

    debugging sparkline chart rendering problem

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -3,7 +3,7 @@ import { BsCurrencyDollar } from 'react-icons/bs';
 import { FiSettings } from 'react-icons/fi';
 import { GoPrimitiveDot } from 'react-icons/go';
 
-import { stacked, Pie, Button, SparkLine } from '../components';
+import { Stacked, Pie, Button, SparkLine } from '../components';
 
 import { earningData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
