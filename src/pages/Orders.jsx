25665d4297080570a430f8e861216f2b29eae11a
MMORONTO <t2wycemo@gmail.com>
2022-06-22 15:14:24 -0500

    set up Orders and Header components.

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContexMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
 
+import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
+import { Header } from '../components';
+
 const Orders = () => {
   return (
-    <div>Orders</div>
+    <div>
+      <Header category='Page' title='Orders' />
+    </div>
   )
 }
 
