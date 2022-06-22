9260234f8bf0ec91ce909f90fee68a09ab5a9167
MMORONTO <t2wycemo@gmail.com>
2022-06-22 15:46:21 -0500

    layout & style adjustments

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContexMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
 
 import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
 import { Header } from '../components';
@@ -11,12 +11,14 @@ const Orders = () => {
       <GridComponent
         id='gridcomp'
         dataSource={ordersData}
+        allowPaging
       >
         <ColumnsDirective>
           {ordersGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
           ))}
         </ColumnsDirective>
+        <Inject services={[ Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport ]} />
       </GridComponent>
     </div>
   )
