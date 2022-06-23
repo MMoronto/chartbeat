6f8cf90f9c3075fc231c367be8b08d33abdfe148
MMORONTO <t2wycemo@gmail.com>
2022-06-23 10:48:16 -0500

    import required components & data

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Search, Inject } from '@syncfusion/ej2-react-grids';
 
-import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
+import { employeesData, employeesGrid } from '../data/dummy';
 import { Header } from '../components';
 
 const Employees = () => {
@@ -10,16 +10,16 @@ const Employees = () => {
       <Header category='Page' title='Orders' />
       <GridComponent
         id='gridcomp'
-        dataSource={ordersData}
+        dataSource={employeesData}
         allowPaging
         allowSorting
       >
         <ColumnsDirective>
-          {ordersGrid.map((item, index) => (
+          {employeesGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
           ))}
         </ColumnsDirective>
-        <Inject services={[ Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport ]} />
+        <Inject services={[ Page ]} />
       </GridComponent>
     </div>
   )
