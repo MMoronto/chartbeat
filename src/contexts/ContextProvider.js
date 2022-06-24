63ab49c67c92abc92ccbc9a54c81655075ff8f7d
MMORONTO <t2wycemo@gmail.com>
2022-06-24 16:41:54 -0500

    set logic for thememode and color mode functions

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -13,6 +13,20 @@ export const ContextProvider = ({ children }) => {
     const [activeMenu, setActiveMenu] = useState (true);
     const [isClicked, setIsClicked] = useState(initialState);
     const [screenSize, setScreenSize] = useState(undefined);
+    const [currentColor, setCurrentColor] = useState('#03C9D7');
+    const [currentMode, setCurrentMode] = useState('Light');
+
+    const setMode = (e) => {
+        setCurrentMode(e.target.value);
+
+        localStorage.seItem('themeMode', e.target.value);
+    }
+
+    const setColor = (e) => {
+        setCurrentColor(e.target.value);
+
+        localStorage.seItem('colorMode', e.target.value);
+    }
 
     const handleClick = (clicked) => {
         setIsClicked({ ...initialState, [clicked]: true});
