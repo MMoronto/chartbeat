382110134757853334cf3030a3c9577ec361d398
MMORONTO <t2wycemo@gmail.com>
2022-06-24 16:31:14 -0500

    set statecontext

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -7,6 +7,7 @@ import { themeColors } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const ThemeSettings = () => {
+  const { setColor, setMode, currentMode, currentColor, setThemeSettings } = useStateContext();
   return (
     <div className='bg-half-transparent w-screen fixed nav-item top-0 right-0'>
       <div className='float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400'>
@@ -67,7 +68,7 @@ const ThemeSettings = () => {
                     style={{ backgroundColor: item.color }}
                     onClick={() => {}}
                   >
-                    <BsCheck className={`ml-2 text-2xl text-white ${true  ? 'block' : 'hidden'}`} />
+                    <BsCheck className={`ml-2 text-2xl text-white ${false  ? 'block' : 'hidden'}`} />
                   </button>
                 </div>
               </TooltipComponent>
