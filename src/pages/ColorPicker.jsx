6606ca51606461b9191b57dd165d49eb41dc2295
MMORONTO <t2wycemo@gmail.com>
2022-06-24 12:11:11 -0500

    apply color change to page element

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -3,6 +3,10 @@ import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs';
 
 import { Header } from '../components';
 
+const change = (args) => {
+  document.getElementById('preview').style.backgroundColor = args.currentValue.hex;
+}
+
 const ColorPicker = () => {
   return (
     <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
