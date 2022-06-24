1cec08e3a06d4651bb5022aebeaf3fb565f6eef4
MMORONTO <t2wycemo@gmail.com>
2022-06-24 12:40:51 -0500

    fix bug. render theme settings side panel

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -15,8 +15,9 @@ const ThemeSettings = () => {
             type='button'
             onClick={() => {}}
             style={{ color: 'rgb(153, 171, 180)', borderRadius: '50%' }}
+            className='text-2xl p-3 hover:drop-shadow-xl hover:bg-light-gray'
           >
-
+            <MdOutlineCancel />
           </button>
         </div>
       </div>
