a2e4aefea0c1148bce4c2b382b72be15d717ec24
MMORONTO <t2wycemo@gmail.com>
2022-06-24 16:57:56 -0500

    make theme settings dynamic/hideable

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { useStateContext } from './contexts/ContextProvider';
 import './App.css'
 
 const App = () => {
-  const { activeMenu } = useStateContext();
+  const { activeMenu, themeSettings } = useStateContext();
 
   return (
     <div>
@@ -41,7 +41,7 @@ const App = () => {
                 {/* </div> */}
 
                 <div>
-                    <ThemeSettings />
+                    {themeSettings && <ThemeSettings />}
 
                     <Routes>
                         {/*  Dashboard  */}
