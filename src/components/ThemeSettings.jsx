e4c62815fdb2cc403f8b5682782e8626e870e5da
MMORONTO <t2wycemo@gmail.com>
2022-06-27 11:52:51 -0500

    debugging side bar render issue

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -10,7 +10,7 @@ const ThemeSettings = () => {
   const { setColor, setMode, currentMode, currentColor, setThemeSettings } = useStateContext();
   return (
     <div className='bg-half-transparent w-screen fixed nav-item top-0 right-0'>
-      <div className='float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400'>
+      <div className='float-right h-screen dark:text-gray-200 bg-white dark:bg-[#484B52] w-400'>
         <div className='flex justify-between items-center p-4 ml-4'>
           <p className='font-semibold text-xl'>Settings</p>
           <button
@@ -61,14 +61,17 @@ const ThemeSettings = () => {
           <div className='flex gap-3'>
             {themeColors.map((item, index) => (
               <TooltipComponent key={index} content={item.name} position='TopCenter'>
-                <div className='relative mt-2 cursor-pointer flex gap-5 items-center'>
+                <div 
+                  className='relative mt-2 cursor-pointer flex gap-5 items-center'
+                  key={item.name}
+                >
                   <button
                     type='button'
                     className='h-10 w-10 rounded-full cursor-pointer'
                     style={{ backgroundColor: item.color }}
-                    onClick={() => {}}
+                    onClick={() => setColor(item.color)}
                   >
-                    <BsCheck className={`ml-2 text-2xl text-white ${false  ? 'block' : 'hidden'}`} />
+                    <BsCheck className={`ml-2 text-2xl text-white ${item.color === currentColor  ? 'block' : 'hidden'}`} />
                   </button>
                 </div>
               </TooltipComponent>
