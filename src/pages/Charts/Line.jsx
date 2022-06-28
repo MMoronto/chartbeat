a9e99cd342bc3a56d20c445e8e305ab31d5bda73
MMORONTO <t2wycemo@gmail.com>
2022-06-27 21:43:33 -0500

    set up line component

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -5,7 +5,10 @@ import { Header, LineChart } from '../../components';
 const Line = () => {
   return (
     <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
-      <Header />
+      <Header category='Chart' title='Inflation Rate' />
+      <div className='w-full'>
+        
+      </div>
     </div>
   )
 }
