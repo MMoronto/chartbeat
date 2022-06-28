015d9fc0def26a5406604f25f6de3daf98819920
MMORONTO <t2wycemo@gmail.com>
2022-06-28 11:51:31 -0500

    set up bar chart component. import syncfusion & local files

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, BarSeries, DateTime, Legend, Tooltip } from '@syncfusion/ej2-react-charts';
+
+import { barCustomSeries, barPrimaryYAxis, barPrimaryXAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
 
 const Bar = () => {
+  const { currentMode } = useStateContext();
+
   return (
-    <div>Bar</div>
+    <ChartComponent
+      id='bar-chart'
+      height='420px'
+      primaryXAxis={barPrimaryXAxis}
+      primaryYAxis={barPrimaryYAxis}
+      chartArea={{ border: { width: 0 }}}
+      tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+    >
+      <Inject services={[BarSeries,DateTime, Legend, Tooltip]}/>
+      <SeriesCollectionDirective>
+        {barCustomSeries.map((item, index) => 
+        <SeriesDirective key={index} {...item} />)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   )
 }
 
