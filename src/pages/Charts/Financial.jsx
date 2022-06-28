c08f5987c20e51746730f51d7634bec2f1701b49
MMORONTO <t2wycemo@gmail.com>
2022-06-28 15:24:40 -0500

    set up financial charts component. import syncfusion & local files

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, HiloSeries, DateTime, Zoom, Logarithmic, Crosshair, Tooltip } from '@syncfusion/ej2-react-charts';
+import { financialChartData, FinancialPrimaryXAxis, FinancialPrimaryYAxis } from '../../data/dummy';
+import { ChartsHeader } from '../../components';
+import { useStateContext } from '../../contexts/ContextProvider';
+
+const date1 = new Date('2017, 1, 1');
 
 const Financial = () => {
+  const { currentMode } = useStateContext();
+
   return (
-    <div>Financial</div>
+    <ChartComponent
+      id='line-chart'
+      height='420px'
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxis}
+      chartArea={{ border: { width: 0 }}}
+      tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+    >
+      <Inject services={[LineSeries,DateTime, Legend, Tooltip]}/>
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, index) => 
+        <SeriesDirective key={index} {...item} />)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   )
 }
 
