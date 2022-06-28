517ffa07d206b52972eabd5533859d611296ce2d
MMORONTO <t2wycemo@gmail.com>
2022-06-28 15:39:41 -0500

    setup financial chart component. layout adjustments

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -6,26 +6,45 @@ import { useStateContext } from '../../contexts/ContextProvider';
 
 const date1 = new Date('2017, 1, 1');
 
+function filterValue(value) {
+  if (value.x >= date1) {
+    return value.x, value.high, value.low;
+  }
+}
+const returnValue = financialChartData.filter(filterValue);
+
 const Financial = () => {
   const { currentMode } = useStateContext();
 
   return (
-    <ChartComponent
-      id='line-chart'
-      height='420px'
-      primaryXAxis={LinePrimaryXAxis}
-      primaryYAxis={LinePrimaryYAxis}
-      chartArea={{ border: { width: 0 }}}
-      tooltip={{ enable: true }}
-      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
-    >
-      <Inject services={[LineSeries,DateTime, Legend, Tooltip]}/>
-      <SeriesCollectionDirective>
-        {lineCustomSeries.map((item, index) => 
-        <SeriesDirective key={index} {...item} />)}
-      </SeriesCollectionDirective>
-    </ChartComponent>
-  )
-}
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <ChartsHeader category='Financial' title='AAPL Historical' />
+      <div className='w-full'>
+        <ChartComponent
+          id='charts'
+          primaryXAxis={FinancialPrimaryXAxis}
+          primaryYAxis={FinancialPrimaryYAxis}
+          chartArea={{ border: { width: 0 }}}
+          tooltip={{ enable: true, shared: true }}
+          crosshair={{ enable: true, lineType: 'Vertical', line: { width: 0 } }}
+          background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+        >
+          <Inject services={[HiloSeries,DateTime, Logarithmic, Crosshair, Zoom, Tooltip]}/>
+          <SeriesCollectionDirective>
+            <SeriesDirective 
+              dataSource={returnValue}
+              xName='x'
+              yName='low'
+              name='Apple Inc'
+              type='Hilo'
+              low='low'
+              high='high' 
+            />
+          </SeriesCollectionDirective>
+        </ChartComponent>
+        </div>
+    </div>
+  );
+};
 
-export default Financial
+export default Financial;
