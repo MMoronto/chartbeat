8be2e31166b4755ba5e2badbbbedf32de07a33c4
MMORONTO <t2wycemo@gmail.com>
2022-06-29 12:43:04 -0500

    customize chart layout

--- src/pages/Charts/ColorMapping.jsx
+++ src/pages/Charts/ColorMapping.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, SplineAreaSeries, DateTime, Legend } from '@syncfusion/ej2-react-charts';
-import { Header } from '../../components';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, ColumnSeries, Category, Tooltip, RangeColorSettingDirective, RangeColorSettingsDirective, Legend } from '@syncfusion/ej2-react-charts';
 
-import { areaCustomSeries, areaPrimaryYAxis, areaPrimaryXAxis } from '../../data/dummy';
+import { colorMappingData, rangeColorMapping, ColorMappingPrimaryYAxis, ColorMappingPrimaryXAxis } from '../../data/dummy';
+
+import { Header } from '../../components';
 import { useStateContext } from '../../contexts/ContextProvider';
 
 const ColorMapping = () => {
@@ -10,22 +11,37 @@ const ColorMapping = () => {
 
   return (
     <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
-      <Header category='Area' title='Inflation Rate by Percentage' />
-      <ChartComponent
-        id='area-chart'
-        height='420px'
-        primaryXAxis={areaPrimaryXAxis}
-        primaryYAxis={areaPrimaryYAxis}
-        chartArea={{ border: { width: 0 }}}
-        tooltip={{ enable: true }}
-        background={currentMode === 'Dark' ? '#33373E' : '#fff'}
-      >
-        <Inject services={[SplineAreaSeries,DateTime, Legend]}/>
-        <SeriesCollectionDirective>
-          {areaCustomSeries.map((item, index) => 
-          <SeriesDirective key={index} {...item} />)}
-        </SeriesCollectionDirective>
-      </ChartComponent>
+      <Header category='Color Mapping' title='USA CLIMATE - WEATHER BY MONTH' />
+      <div className='w-full'>
+        <ChartComponent
+          id='charts'
+          height='420px'
+          primaryXAxis={ColorMappingPrimaryXAxis}
+          primaryYAxis={ColorMappingPrimaryYAxis}
+          chartArea={{ border: { width: 0 }}}
+          legendSettings={{ mode: 'Range', background: 'white' }}
+          tooltip={{ enable: true }}
+          background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+        >
+          <Inject services={[ColumnSeries, Tooltip, Category, Legend]}/>
+          <SeriesCollectionDirective> 
+            <SeriesDirective 
+              dataSource={colorMappingData[0]}
+              name='USA'
+              xName='x'
+              yName='y'
+              type='Column'
+              cornerRadius={{
+                topLeft: 10,
+                topRight: 10,
+              }}
+            />
+          </SeriesCollectionDirective>
+          <RangeColorSettingsDirective>
+            {rangeColorMapping.map((item, index) => <RangeColorSettingDirective key={index} {...item} />)}
+          </RangeColorSettingsDirective>
+        </ChartComponent>
+      </div>
     </div>
   )
 }
